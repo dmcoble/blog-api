fdb4078e7f908bd02622a0d30b20a1abe9f511be
Dallas Coble <dc@dallascoble.com>
2018-03-14 21:30:32 -0700

    added test for listing articles

--- test/test.js
+++ test/test.js
@@ -22,26 +22,51 @@ describe('loading express', function () {
   afterEach(function (done) {
     server.close(done);
   });
+ 
   it('responds to /api/', function testSlash(done) {
-  request(server)
-    .get('/api/')
-    .expect(200, done);
+    request(server)
+      .get('/api/')
+      .expect(200, done);
   });
+
   it('Post an article to /api/articles/', function testSlash(done) {
-  request(server)
-    .post('/api/articles')
-    .send({"article": articleReq}) 
-    .expect(200, done);
+    request(server)
+      .post('/api/articles')
+      .send({"article": articleReq}) 
+      .expect(200, done);
   });
+
   it('Get an articles content from /api/articles/:id', function testSlash(done) {
-  // populate article
-  request(server)
-    .post('/api/articles')
-    .send({"article": articleReq}) 
-  // get article and compare content
-  request(server)
-    .get('/api/articles/1')
-    .expect(200, contentRes, done)
+    // populate article
+    request(server)
+      .post('/api/articles')
+      .send({"article": articleReq}) 
+    // get article and compare content
+    request(server)
+      .get('/api/articles/0')
+      .expect(200, contentRes, done)
+  });
+
+  it('Get list of articles from /api/articles', function testSlash(done) {
+    this.timeout(10 * 1000);
+    // populate 50 articles
+    
+    for (i = 0; i < 50; i++) {
+      (function(n) {
+        request(server)
+          .post('/api/articles')
+          .send({"article": articleReq}) 
+      })(i);
+    }
+    // see if I get 20 articles back 
+    request(server)
+      .get('/api/articles')
+      .expect(returnsList)
+      .end(done);
+
+    function returnsList(res) {
+      if (res.body.articleNum == 20 ) throw new Error(res.body.articleNum);
+    }
   });
 
 });
